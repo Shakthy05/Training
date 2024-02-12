3955bb51ef8e58cf0ee92464f8ebc4b24c8ef4cd
Shakthy05 <shakthy2002@gmail.com>
2024-02-12 21:10:43 +0530

    Number conversion game

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+while (true) {
+   WriteLine ("\nEnter a number :");
+   if (int.TryParse (Console.ReadLine (), out int num)) {
+      WriteLine ("Do you want convert to (B)inary or (H)exadecimal?");
+      switch (ReadKey ().Key) {
+         case (ConsoleKey.B):
+            WriteLine ("\n" + Convert.ToString (num, 2));
+            break;
+         case (ConsoleKey.H):
+            WriteLine ($"\n{num:X}");
+            break;
+         default:
+            WriteLine ("Enter a valid number");
+            break;
+      }
+   }
+   WriteLine ("Do you want to convert again? (Y)es or (No)");
+   if (ReadKey ().Key == ConsoleKey.Y) continue;
+   else break;
+}
