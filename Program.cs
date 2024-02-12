a7ffe97f140d5b216aa9b6b0a1246af6ff8da9ff
Shakthy05 <shakthy2002@gmail.com>
2024-02-12 21:28:35 +0530

    Print diamond pattern

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Program to Print Diamond
+Console.WriteLine ("Enter no of lines: ");
+int n = int.Parse (Console.ReadLine () ?? "");
+int space = n;
+int condition = (n * 2) + 1;
+for (int i = 1; i <= condition; i += 2) {
+   Console.WriteLine (string.Concat (Enumerable.Repeat (" ", space)) + string.Concat (Enumerable.Repeat ("*", i)));
+   space--;
+}
+int blank = 1;
+for (int j = condition - 2; j >= 1; j -= 2) {
+   Console.WriteLine (string.Concat (Enumerable.Repeat (" ", blank)) + string.Concat (Enumerable.Repeat ("*", j)));
+   blank++;
+}
