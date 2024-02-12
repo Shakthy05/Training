73eab6a85eeab109601fe2e9a5a4538f4820ac7c
Shakthy05 <shakthy2002@gmail.com>
2024-02-12 21:21:09 +0530

    Prime number

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Program to check prime number
+Console.WriteLine ("Enter a number: ");
+int.TryParse (Console.ReadLine () ?? "", out int num);
+int i;
+if (num == 1)
+   Console.WriteLine ("1 is neither Prime or Composite");
+else {
+   int Limit = num / 2;
+   for (i = 2; i <= Limit; i++) {
+      if (num % i == 0) {
+         Console.WriteLine ("The number is not a prime number");
+         break;
+      }
+   }
+   if (i > Limit) Console.WriteLine ("The number is a prime number");
+}
