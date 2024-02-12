bd8faab74433bd81f883bfc37df8f257214001e0
Shakthy05 <shakthy2002@gmail.com>
2024-02-12 21:40:31 +0530

    Isogram

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Program to Check for ISOGRAM
+using static System.Console;
+WriteLine ("Enter a string: ");
+string isogram = ReadLine () ?? "";
+bool result = CheckIsogram (isogram);
+WriteLine ("The String is " + (result == true ? "" : "not ") + "an Isogram");
+bool CheckIsogram (string Isogram) {
+   return Isogram.Distinct ().Count () == Isogram.Length;
+}
