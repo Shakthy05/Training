6859a7b7db9b49e61c13a83a58f50529fb44f5d1
Shakthy05 <shakthy2002@gmail.com>
2024-02-12 21:39:30 +0530

    Reduced String

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Program to display Reduced String
+Console.WriteLine ("Enter a string: ");
+var Input = Console.ReadLine () ?? "";
+List<string> Output = new List<string> ();
+(int, int) CountChar (string Input) {
+   int c = 0, i = 0;
+   for (i = 0; i < Input.Length - 1; i++) {
+      if (Input[i] == Input[i + 1]) c++;
+      else break;
+   }
+   return (c, i);
+}
+while (Input.Length != 0) {
+   (int count, int j) = CountChar (Input);
+   if (count % 2 == 0) Output.Add (Input[j].ToString ());
+   Input = Input.Substring (j + 1);
+}
+Output.ForEach (Console.Write);
