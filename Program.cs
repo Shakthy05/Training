73a3b672815efff9f214509e6b8cd8a884c5b463
Shakthy05 <shakthy2002@gmail.com>
2024-02-12 21:49:22 +0530

    Factorial and intergral part of a number

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Program to  DISPLAY THE INDIVIDUAL DIGITS OF A GIVEN NUMBER 
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
+using static System.Console;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+WriteLine ("Enter a number: ");
+if (double.TryParse (ReadLine () ?? "", out double Input)) {
+   string Output = Input.ToString ();
+   GetNumbers (Output);
+} else WriteLine ("Enter a valid number");
+void GetNumbers (string Number) {
+   var Integral = new List<string> ();
+   var Decimal = new List<string> ();
+   foreach (var ch in Number) {
+      if (ch.ToString () != ".") {
+         Integral.Add (ch.ToString ());
+         continue;
+      } else if (ch.ToString () == ".") {
+         int Length = Number.IndexOf (ch) + 1;
+         for (int i = Length; i < Number.Length; i++) Decimal.Add (Number[i].ToString ());
+      }
+      break;
+   }
+   Write ("The Integral Part is : ");
+   Integral.ForEach (num => Write (num + " "));
+   Write ("\nThe Fractional Part is : ");
+   Decimal.ForEach (num => Write (num + " "));
+}
