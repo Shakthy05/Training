e125052e3b7f705c869b6e39a0e6e2b151b92535
Shakthy05 <shakthy2002@gmail.com>
2024-02-12 21:54:05 +0530

    Swap indices

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Program to Swap Indices of the Random Number Generated
+using static System.Console;
+WriteLine ("The Number Generated: ");
+Random random = new Random ();
+string num = random.Next ().ToString ();
+WriteLine (num);
+Write ("Enter the Indices to be Swapped: ");
+int.TryParse (ReadLine () ?? "", out int index1);
+int.TryParse (ReadLine () ?? "", out int index2);
+SwapIndices (num, index1, index2);
+
+void SwapIndices (string Input, int a, int b) {
+   char[] result = Input.ToCharArray ();
+   (result[a], result[b]) = (result[b], result[a]);
+   for (int j = 0; j < result.Length; j++) Write (result[j]);
+}
