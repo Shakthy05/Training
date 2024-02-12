ffa0ab9f011e1723ac4708649d80386ffc480ac6
Shakthy05 <shakthy2002@gmail.com>
2024-02-12 21:56:20 +0530

    Voting contest

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//VOTING CONTEST
+//Program to get votes in a string and return the winner with maximum number of votes
+//Sample Input :Helloworld
+//Sample Output :L, 3 => Winner and votes 
+using static System.Console;
+while (true) {
+   Write ("Give your vote(s): ");
+   string votes = ReadLine ().ToLower ().Replace (" ", "");
+   if (!string.IsNullOrEmpty (votes) && votes.All (Char.IsLetter)) {
+      var (c, v) = GetWinner (votes);
+      WriteLine ($"The Winner is {c} with {v} vote(s)");
+      break;
+   } else Write ("Enter a valid vote\n");
+}
+
+///<summary>Function getWinner to return the winner with number of votes </summary>
+///<param name="votes">votes given as string</param>
+(char Candidate, int Votes) GetWinner (string votes)
+   => votes.GroupBy (x => x)
+          .Select (x => (candidate: x.Key, votes: x.Count ()))
+          .OrderByDescending (x => x.votes)
+          .FirstOrDefault ();
