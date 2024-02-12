2ec25693321a1645fc1f74c4e642567fd5c66a91
Shakthy05 <shakthy2002@gmail.com>
2024-02-12 21:42:25 +0530

    ABECEDERIAN word

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Program to Check LONGEST ABECEDARIAN WORD 
+Console.WriteLine ("Enter the number of words to check for Abecedarian word: ");
+if (int.TryParse (Console.ReadLine (), out int Terms)) {
+   string[] Words = new string[Terms];
+   for (int i = 0; i < Terms; i++) {
+      Console.WriteLine ($"Enter word {i + 1}: ");
+      Words[i] = Console.ReadLine () ?? "";
+   }
+   Console.WriteLine ($"The Longest Abecedarian Word is : {GetAbecedarian (Words)}");
+} else Console.WriteLine ("Enter Valid Input");
+
+string GetAbecedarian (string[] Words) {
+   string[] SortedArray = new string[Words.Length];
+   List<string> Sorted = new List<string> ();
+   for (int i = 0; i < Words.Length; i++) {
+      char[] charArray = Words[i].ToCharArray ();
+      Array.Sort (charArray);
+      SortedArray[i] = new string (charArray);
+      if (SortedArray[i] == Words[i])
+         Sorted.Add (SortedArray[i]);
+   }
+   string LongestWord = Sorted.OrderByDescending (x => x.Length).First ();
+   return LongestWord;
+}
