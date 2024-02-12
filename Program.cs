741b35f304c9dc8709f06e0ca6224ecec48d8ef6
Shakthy05 <shakthy2002@gmail.com>
2024-02-12 21:33:39 +0530

    Reverse a given number

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Program to Reverse a given number
+using static System.Console;
+WriteLine ("Enter a number: ");
+string Number = ReadLine () ?? "";
+int i, numberLength = Number.Length;
+int condition = numberLength / 2;
+for (i = 0; i <= condition; i++)
+   if (Number[i] != Number[numberLength - 1 - i])
+      break;
+WriteLine ("It is " + (i == condition + 1 ? "" : "not ") + "a palindrome");
