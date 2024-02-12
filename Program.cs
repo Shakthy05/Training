09484ad01fe495364232d039668b7b279a2a545c
Shakthy05 <shakthy2002@gmail.com>
2024-02-12 21:36:02 +0530

    Pascal's Triangle

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Program to Print Pascal's Triangle
+using static System.Console;
+WriteLine ("Enter number of rows: ");
+int rows = int.Parse (ReadLine () ?? "");
+int val = 0;
+for (int i = 0; i < rows; i++) {
+   for (int blank = 0; blank <= rows - i; blank++)
+      Write ("   ");
+   for (int j = 0; j <= i; j++) {
+      if (i == 0 || j == 0 || i == j) val = 1;
+      else {
+         val = val * (i - j + 1) / j;
+      }
+      Write (val + "     ");
+   }
+   WriteLine ();
+}
