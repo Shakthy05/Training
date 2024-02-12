f42f3bb4350bb2cef38b9778339b5cda5521a545
Shakthy05 <shakthy2002@gmail.com>
2024-02-12 21:16:09 +0530

    Fibonacci series

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine ("Method - 1 Using Recursion");
+Console.WriteLine ("Enter number of terms : ");
+int length = int.Parse (Console.ReadLine () ?? "");
+string term = length.ToString ();
+List<int> series = new List<int> ();
+int a = 0, b = 1, count = 1;
+Fibonacci (a, b, count, length);
+void Fibonacci (int a, int b, int count, int length) {
+   if (count <= length) {
+      Console.Write (a + " ");
+      series.Add (a);
+      Fibonacci (b, a + b, count + 1, length);
+   }
+}
+Console.WriteLine ();
+foreach (var item in series) {
+   Console.Write (item + " ");
+}
+Console.WriteLine ("Method - 2 Using Iteration");
+for (int i = 0; i < length; i++) {
+   Console.Write (a + " ");
+   (a, b) = (b, a + b);
+}
