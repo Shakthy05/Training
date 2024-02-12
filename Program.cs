564d1e14b269ab4fb6e480366b9afa1267fd80d7
Shakthy05 <shakthy2002@gmail.com>
2024-02-12 21:38:28 +0530

    Strong password

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Program to check for Strong Password
+using static System.Console;
+Write ("Enter Password: ");
+string password = ReadLine () ?? "";
+string specials = "!@#$%^&*()-+";
+string checkPassword = "";
+if (password.Length >= 6) {
+   checkPassword += (password.Any (Char.IsDigit) ? "" : "The Password must contain atleast one digit.\n");
+   checkPassword += (password.Any (Char.IsLower) ? "" : "The Password must contain atleast one Lowercase alphabet.\n");
+   checkPassword += (password.Any (Char.IsUpper) ? "" : "The Password must contain atleast one Uppercase alphabet.\n");
+   checkPassword += (password.Any (specials.Contains) ? "" : "The Password must contain atleast one of these !@#$%^&*()-+ special characters.\n");
+   WriteLine ((checkPassword == "" ? "The Password is Strong" : "Invalid password\n" + checkPassword));
+} else WriteLine ("Invalid password. The Password must contain atleast 6 characters.");
