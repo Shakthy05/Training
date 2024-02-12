98afd805e784dbe450385db4fbb0e5ab56d9f64c
Shakthy05 <shakthy2002@gmail.com>
2024-02-12 21:34:49 +0530

    Digital root

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Program to Print Digital Root
+using static System.Console;
+WriteLine ("Enter a number: ");
+int num = int.Parse (ReadLine () ?? "");
+int sum = 0;
+while (num > 0) {
+   int n = num % 10;
+   sum += n;
+   num /= 10;
+}
+if (sum > 10) {
+   int t = sum % 10;
+   sum /= 10;
+   int m = t + sum;
+   WriteLine (m);
+} else WriteLine (sum);
