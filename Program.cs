e3f44ec5489566a531728f09890d525f22a3f5a7
Shakthy05 <shakthy2002@gmail.com>
2024-02-12 21:22:23 +0530

    Palindrome

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine ("Enter a text: ");
+string name = Console.ReadLine () ?? "";
+name = name.ToLower ().Replace (" ", "");
+string reverse = "";
+for (int i = name.Length - 1; i >= 0; i--)
+   reverse += name[i];
+string result = "It is a " + (name == reverse ? "" : "not a") + " palindrome";
+Console.WriteLine (result);
