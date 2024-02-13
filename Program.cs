4c5e3784ad866cc352d4e8be7e02c122bdb1851e
Shakthy05 <shakthy2002@gmail.com>
2024-02-13 19:36:08 +0530

    Expression Evaluator

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ----------------------------------------------------------------------------------------
+// Expression evaluator
+// Implementation of unary operator in the expression evaluator
+// ----------------------------------------------------------------------------------------
+namespace Eval;
+
+class Program {
+   static void Main (string[] args) {
+      var eval = new Evaluator ();
+      for (; ; ) {
+         Console.Write ("> ");
+         string text = Console.ReadLine () ?? "";
+         if (text == "exit") break;
+         try {
+            double result = eval.Evaluate (text);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine (result);
+         } catch (Exception e) {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine (e.Message);
+         }
+         Console.ResetColor ();
+      }
+   }
+}
---
+++ Tokenizer.cs
@@ -0,0 +1,60 @@
+﻿namespace Eval;
+class Tokenizer {
+   public Tokenizer (Evaluator eval, string text) {
+      mText = text; mN = 0; mEval = eval;
+   }
+   readonly Evaluator mEval;  // The evaluator that owns this 
+   readonly string mText;     // The input text we're parsing through
+   int mN;                    // Position within the text
+   public Token Next () {
+      while (mN < mText.Length) {
+         char ch = char.ToLower (mText[mN++]);
+         switch (ch) {
+            case ' ' or '\t': continue;
+            case (>= '0' and <= '9') or '.': return GetNumber ();
+            case '(' or ')': return new TPunctuation (ch);
+            case '+' or '-' or '*' or '/' or '^' or '=': return GetOperator ();
+            case >= 'a' and <= 'z': return GetIdentifier ();
+            default: return new TError ($"Unknown symbol: {ch}");
+         }
+      }
+      return new TEnd ();
+   }
+   Token GetIdentifier () {
+      int start = mN - 1;
+      while (mN < mText.Length) {
+         char ch = char.ToLower (mText[mN++]);
+         if (ch is >= 'a' and <= 'z') continue;
+         mN--; break;
+      }
+      string sub = mText[start..mN];
+      if (mFuncs.Contains (sub)) return new TOpFunction (mEval, sub);
+      else return new TVariable (mEval, sub);
+   }
+   readonly string[] mFuncs = { "sin", "cos", "tan", "sqrt", "log", "exp", "asin", "acos", "atan" };
+   Token GetNumber () {
+      int start = mN - 1;
+      while (mN < mText.Length) {
+         char ch = mText[mN++];
+         if (ch is (>= '0' and <= '9') or '.') continue;
+         mN--; break;
+      }
+      // Now, mN points to the first character of mText that is not part of the number
+      string sub = mText[start..mN];
+      if (double.TryParse (sub, out double f)) return new TLiteral (f);
+      return new TError ($"Invalid number: {sub}");
+   }
+   Token GetOperator () {
+      int start = mN - 1;
+      char ch = mText[start];
+      if (ch is '+' or '-') {
+         if (start is 0 || (mText[--start] is '+' or '-' or '*' or '/' or '^' or '=' or '(' or ' ')) return new TOpUnary (mEval, ch);
+         if (mText[mN] is '+' or '-') {
+            char a = ch == mText[mN] ? '+' : '-';
+            mN++;
+            return new TOpArithmetic (mEval, a);
+         }
+      }
+      return new TOpArithmetic (mEval, ch);
+   }
+}
