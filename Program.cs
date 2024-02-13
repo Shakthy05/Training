6e415322c84e64f52da84c6e8a82aca666ae47e7
Shakthy05 <shakthy2002@gmail.com>
2024-02-13 19:47:15 +0530

    Anagram

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ----------------------------------------------------------------------------------------
+// Program.cs
+// Anagram
+// To print all possible anagrams 
+// ----------------------------------------------------------------------------------------
+var wordsList = File.ReadAllLines ("C:/Users/ajayk/Downloads/words.txt")
+            .GroupBy (x => string.Concat (x.OrderBy (c => c))) // Grouping the anagrams by sorting them 
+            .Where (x => x.Count () > 1) // Eliminating the non anagrams words
+            .OrderByDescending (x => x.Count ()); // Arranging based on the number of anagrams in descending order 
+using (StreamWriter sw = new ("D:/work/test.txt"))
+   foreach (var words in wordsList) sw.WriteLine ($"{words.Count ()} {string.Join (", ", words)}");
